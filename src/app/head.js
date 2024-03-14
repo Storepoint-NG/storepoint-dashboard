1c4b163b54242c7356398148b94fee03acbb1fa9
Haruna Faruk <harunafaruk2004@gmail.com>
2024-03-14 10:36:02 +0100

    commit

---
+++ src/app/head.js
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+
+export default function Head() {
+  return (
+    <Fragment>
+      <link rel="manifest" href="/manifest.json" />
+      {/* Add other head tags like meta, title, etc. as needed  */}
+    </Fragment>
+  );
+}
